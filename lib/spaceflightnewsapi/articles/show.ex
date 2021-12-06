32d4e0c82375a7ff4b363b695b0cf3a1a31df131
wagner <wagncarv@gmail.com>
2021-12-06 19:44:32 -0300

    correcao show

--- lib/spaceflightnewsapi/articles/show.ex
+++ lib/spaceflightnewsapi/articles/show.ex
@@ -1,6 +1,6 @@
 defmodule Spaceflightnewsapi.Articles.Show do
     use Ecto.Schema
-    alias Spaceflightnewsapi.{Article, Counter, Repo}
+    alias Spaceflightnewsapi.{Article, Repo}
     import Ecto.Query
 
     def call(params) do
@@ -11,27 +11,4 @@ defmodule Spaceflightnewsapi.Articles.Show do
     end
 
     defp handle_result(%Scrivener.Page{} = page), do: {:ok, page}
-
-    def paginated_call() do
-        Counter.start_link(:ok)
-        %{limit: limit, offset: offset} = Counter.value()
-        first_page = paginate(limit, offset)
-        result =
-        for _n <- paginate(limit, offset) do
-            Counter.increment()
-            %{limit: limit, offset: offset} = Counter.value()
-            paginate(limit, offset)
-        end
-        Counter.reset()
-        {:ok, first_page ++ result
-        |> List.flatten()}
-    end
-
-    defp paginate(limit, offset) do
-        Article
-        |> offset(^offset)
-        |> limit(^limit)
-        |> preload([:events, :launches])
-        |> Repo.all()
-    end
 end
